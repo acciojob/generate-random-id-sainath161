9222c994a9bf1250721b319afbb8faef28659783
Sainath <71361447+sainath161@users.noreply.github.com>
2024-02-27 14:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabodefghijklmnopqrstuvwxyz0123456789";
+	let result = '';
+	for (let i = 0; i < 1; i++) {
+		result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
 }
 
 // Do not change the code below.
